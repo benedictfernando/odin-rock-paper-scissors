5f684814b4495671ad8bbc6e14b2cca133ae39b2
benedict <fernandobenedict619@gmail.com>
2022-02-17 00:30:29 +0800

    finalize playRound

--- main.js
+++ main.js
@@ -4,14 +4,40 @@ function computerPlay() {
   return choices[Math.floor(Math.random() * choices.length)]
 }
 
+function playerPlay() {
+  let choice = prompt("What's your pick?").toLowerCase();
+
+  while(true) {
+    if (choice == "rock"|| choice == "paper" || choice == "scissors") {
+      return choice;
+    } else {
+      choice = prompt("Please enter valid input only.").toLowerCase();
+    }
+  }
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == "rock" && computerSelection == "scissors") {
+    return "You win~ rock beats scissors!"
+  } else if (playerSelection == "rock" && computerSelection == "paper") {
+    return "You lose~ paper beats rock!"
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    return "You win~ scissors beats paper!"
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    return "You lose~ rock beats scissors!"
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    return "You win~ paper beats rock!"
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    return "You lose~ scissors beats paper!"
+  } else {
+    return "Tie!"
+  }
+}
+
 // create a function called playRound(playerSelection, computerSelection)
   // if ... else statements
     // plus, return statements || 'you lose' or 'you win'
     // else, tie
-
-// initialize a function called playerPlay
-  // prompt the user 
-  // convert variable to lowercase
 
 // initialize two score variables, which will keep track of the scores
   // playerScore & computerScore
