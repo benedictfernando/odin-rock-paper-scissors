07925f024a8c39bfde2626b9594d4b12e52a9a6a
benedict <fernandobenedict619@gmail.com>
2022-02-16 23:39:56 +0800

    initialize boilerplate html & pseudocode

---
+++ main.js
@@ -0,0 +1,26 @@
+// create a function called computerPlay
+  // create the array of choices || rock, paper, scissors
+  // return random choice
+
+// create a function called playRound(playerSelection, computerSelection)
+  // if ... else statements
+    // plus, return statements || 'you lose' or 'you win'
+    // else, tie
+
+// initialize a function called playerPlay
+  // prompt the user 
+  // convert variable to lowercase
+
+// initialize two score variables, which will keep track of the scores
+  // playerScore & computerScore
+
+// have a loop that will iterate the playRound function 5x, called game()
+  // run playerPlay function, then assign its value to a variable
+  // same goes with computerPlay
+  // run playRound, passing these arguments as parameters to the function
+    // increment score of the winning side
+    // if returned string contains 'win', increment playerScore
+
+// print winner, depending on the two score variables
+
+// game()
